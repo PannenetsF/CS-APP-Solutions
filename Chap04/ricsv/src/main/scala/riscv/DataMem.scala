19590a036d5deb4fe598038543bccafd95915ea0
pannenetsf <3111812673@qq.com>
2020-08-04 15:35:53 +0800

    finish chisel3 cpu modules

---
+++ Chap04/ricsv/src/main/scala/riscv/DataMem.scala
@@ -0,0 +1,26 @@
+package riscv
+
+
+import chisel3._
+import chisel3.util._
+import freechips.rocketchip.config.Parameters
+
+class DataMemIo(implicit p: Parameters) extends CoreBundle()(p) {
+  val addr = Input(UInt(xlen.W))
+  val wdata = Input(UInt(xlen.W))
+  val rdata = Output(UInt(xlen.W))
+  val memRead = Input(Bool())
+  val memWrite = Input(Bool())
+}
+
+class DataMem(implicit val p: Parameters) extends Module with CoreParams {
+  val io = IO(new DataMemIo)
+  val mem = Mem(xlen, UInt(xlen.W))
+  when (io.memRead)  {
+    io.rdata := mem(io.addr)
+  }
+  when (io.memWrite) {
+    mem(io.addr) := io.wdata
+  }
+}
+
